9a104118440d2285937b7414175eaf97a7348624
Yetocome <250745820@qq.com>
2017-07-27 17:53:40 +0800

    Small fixed

---
+++ baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?f0f019a2df4f2574ab7c28cce8c2c50e";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
