c60cd0735d96204fcf613eab6592aa6d0471326a
Minghao Xie <xmh@eduroam-169-233-218-120.ucsc.edu>
2018-10-07 22:34:54 -0700

    Post a new article, delete redandent files

--- baidu.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?f0f019a2df4f2574ab7c28cce8c2c50e";
-  var s = document.getElementsByTagName("script")[0];
-  s.parentNode.insertBefore(hm, s);
-})();
